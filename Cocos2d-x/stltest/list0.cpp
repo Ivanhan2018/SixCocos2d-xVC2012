8560603a0b97d34a4870ef9db6689b2fdd9a7ccc
HEMEI-20180102W\Administrator <hanxiaohua85@163.com>
2019-08-28 09:23:01 +0800

    STL中的双向链表

---
+++ Cocos2d-x/stltest/list0.cpp
@@ -0,0 +1,25 @@
+﻿#include<list>
+#include<iostream>
+#include<functional>
+int main()
+{
+using namespace std;
+list<int>::iterator Iter;
+#if 1
+list<int> the_list={3,4,6,2,1};//统一的初始化方法(VC2012不支持，VC2013支持)
+#else
+list<int> the_list;
+int temp;
+for( int i = 0; i < 5; i++ )
+{
+cin>>temp;
+the_list.push_front(temp);
+}
+#endif
+the_list.sort(less<int>());//升序排列
+//the_list.sort(greater<int>());//降序排列
+cout<<"the_list=";
+for(Iter=the_list.begin();Iter!=the_list.end();Iter++)
+cout<< " "<<*Iter;
+cout<<endl;
+}
